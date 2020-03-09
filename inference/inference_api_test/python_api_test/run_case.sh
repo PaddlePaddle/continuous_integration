6149a9feac9bd147b8dafaccd39f55327942601f
Peihan <lphs1234567@gmail.com>
2020-03-09 18:13:35 +0800

    add python inference case start script

---
+++ inference/inference_api_test/python_api_test/run_case.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ./Data
+wget https://sys-p0.bj.bcebos.com/inference/python-infer.tgz
+tar -xvf python-infer.tgz
+cd -
+
+rm -rf result
+mkdir result
+export CUDA_VISIBLE_DEVICES=0
+
+cases="test_resnet_fluid.py"
+
+for file in ${cases}
+do
+    echo "====> ${file} case start"
+    python -m nose -s -v --with-xunit --xunit-file=result/${file}_single.xml ${file}
+    echo "====> ${file} case finish"
+done

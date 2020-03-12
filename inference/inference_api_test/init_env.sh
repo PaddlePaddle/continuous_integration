0555a7ad700ef9aa86c9d700f2f3767e91f3ffb9
Liu Xudong <45041955+chalsliu@users.noreply.github.com>
2020-03-12 11:44:48 +0800

    Update init_env.sh

--- inference/inference_api_test/init_env.sh
+++ inference/inference_api_test/init_env.sh
@@ -6,3 +6,5 @@ export PATH=/opt/_internal/cpython-3.7.0/bin/:${PATH}
 export PYTHON_FLAGS="-DPYTHON_EXECUTABLE:FILEPATH=/opt/_internal/cpython-3.7.0/bin/python3.7 -DPYTHON_INCLUDE_DIR:PATH=/opt/_internal/cpython-3.7.0/include/python3.7m -DPYTHON_LIBRARIES:FILEPATH=/opt/_internal/cpython-3.7.0/lib/libpython3.7m.so"
 export PYTHON_ABI="cp37-cp37m"
 export PY_VERSION=3.7
+
+test travis

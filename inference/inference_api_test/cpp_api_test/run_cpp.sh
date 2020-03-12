eb36456f7125eace48a476004a6d6028f61b73aa
Peihan <lphs1234567@gmail.com>
2020-03-12 11:52:27 +0800

    fix auto-test directory problems

--- inference/inference_api_test/cpp_api_test/run_cpp.sh
+++ inference/inference_api_test/cpp_api_test/run_cpp.sh
@@ -3,7 +3,7 @@
 echo ${PADDLE_ROOT}
 export CUDA_LIB="/usr/local/cuda-10.0/targets/x86_64-linux/lib"
 
-sh build.sh "${PADDLE_LIB}/build/fluid_inference_install_dir" ${CUDA_LIB}
+sh build.sh "${PADDLE_ROOT}/build/fluid_inference_install_dir" ${CUDA_LIB}
 if [ $? -ne 0 ];then
     echo -e "\033[33m build test case failed! \033[0m";exit;
 fi
--- inference/inference_api_test/run_inference_case.sh
+++ inference/inference_api_test/run_inference_case.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-sh build_paddel.sh
-source init_paddle.sh
+sh build_py37_paddel.sh
+source init_env.sh
 
 ROOT=`dirname "$0"`
 ROOT=`cd "$ROOT/.."; pwd`
-CPP_ROOT=${ROOT}/cpp_api_test
-PY_ROOT=${ROOT}/python_api_test
+CPP_ROOT=${ROOT}/inference_api_test/cpp_api_test
+PY_ROOT=${ROOT}/inference_api_test/python_api_test
 
-export PADDLE_ROOT=${ROOT}/Paddle
+export PADDLE_ROOT=${ROOT}/inference_api_test/Paddle
 
-cd CPP_ROOT
+cd ${CPP_ROOT}
 sh run_cpp.sh
 
-cd PY_ROOT
+cd ${PY_ROOT}
 sh run_case.sh

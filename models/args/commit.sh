1a9110f2d04293e023de9e413f81322dfe3f2cb4
chalsliu <craziecharles@gmail.com>
2020-06-12 16:38:41 +0800

    Fix conflict

--- models/args/commit.sh
+++ models/args/commit.sh
@@ -4,10 +4,10 @@ cd models
 git rev-parse HEAD >commit_id
 git rev-parse HEAD^^^ >commit_id_head
 git diff `cat commit_id_head` | grep "diff --git" | awk -F ' b/' '{print $2}'  >change_info
-git rev-parse HEAD
-git rev-parse HEAD^^^
-git diff `git rev-parse HEAD^^^` | grep "diff --git" | awk -F ' b/' '{print $2}'
-git diff `cat commit_id_head` | grep "diff --git" | awk -F ' b/' '{print $2}'
+#git rev-parse HEAD
+#git rev-parse HEAD^^^
+#git diff `git rev-parse HEAD^^^` | grep "diff --git" | awk -F ' b/' '{print $2}'
+#git diff `cat commit_id_head` | grep "diff --git" | awk -F ' b/' '{print $2}'
 
 #python commit.py commit_info >change_info
 python models.py change_info args_batch.conf >models_info 

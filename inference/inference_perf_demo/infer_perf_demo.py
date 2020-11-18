9ec7bee2a893d8a89c41a8ab85fe7b0ea94eb586
lipeihan01 <lipeihan01@baidu.com>
2020-11-18 20:46:39 +0800

    add copy right header

--- inference/inference_perf_demo/infer_perf_demo.py
+++ inference/inference_perf_demo/infer_perf_demo.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import argparse
 import time
@@ -7,8 +20,8 @@ import paddle.fluid.inference as paddle_infer
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, help="model filename")
-    parser.add_argument("--model_type", type=str, help="model filename")
+    parser.add_argument("--model_name", type=str, help="model name")
+    parser.add_argument("--model_type", type=str, help="model generate type")
     parser.add_argument("--model_file", type=str, help="model filename")
     parser.add_argument("--params_file", type=str, help="parameter filename")
 
@@ -20,22 +33,24 @@ def parse_args():
     parser.add_argument("--warmup_times", type=int, default=0, help="warmup")
     parser.add_argument("--repeats", type=int, default=1, help="repeats")
     parser.add_argument(
-        "--cpu_math_library_num_threads", type=int, default=1, help="math_thread_num")
-
+        "--cpu_math_library_num_threads",
+        type=int,
+        default=1,
+        help="math_thread_num")
 
     return parser.parse_args()
 
 
 def prepare_config(args):
-    config = paddle_infer.Config(args.model_file, 
-                                 args.params_file)
+    config = paddle_infer.Config(args.model_file, args.params_file)
     if (args.use_gpu):
         config.enable_use_gpu(100, 0)
         if (args.use_trt):
             config.enable_tensorrt_engine()
     else:
         config.disable_gpu()
-        config.set_cpu_math_library_num_threads(args.cpu_math_library_num_threads)
+        config.set_cpu_math_library_num_threads(
+            args.cpu_math_library_num_threads)
         if (args.use_mkldnn):
             config.enable_mkldnn()
     return config
@@ -60,7 +75,7 @@ def Inference(args, predictor):
     time2 = time.time()
     output_data = output_hanlde.copy_to_cpu()
 
-    total_inference_cost = (time2 - time1) * 1000 # 总时延，ms
+    total_inference_cost = (time2 - time1) * 1000  # total latency, ms
 
     return total_inference_cost
 
@@ -73,23 +88,27 @@ def run_demo():
     total_time = Inference(args, predictor)
 
     print("----------------------- Model info ----------------------")
-    print("Model name: {}, Model type: {}".format(args.model_name, args.model_type))
+    print("Model name: {}, Model type: {}".format(args.model_name,
+                                                  args.model_type))
     print("----------------------- Data info -----------------------")
-    print("Batch size: {}, Num of samples: {}".format(args.batch_size, args.repeats))
+    print("Batch size: {}, Num of samples: {}".format(args.batch_size,
+                                                      args.repeats))
     print("----------------------- Conf info -----------------------")
-    print("device: {}, ir_optim: {}".format("gpu" if config.use_gpu() else "cpu",
-                                            config.ir_optim()))
+    print("device: {}, ir_optim: {}".format("gpu" if config.use_gpu() else
+                                            "cpu", config.ir_optim()))
     if (args.use_gpu):
         if (args.use_trt):
-            print("enable_tensorrt: {}".format(config.tensorrt_engine_enabled()))
+            print("enable_tensorrt: {}".format(config.tensorrt_engine_enabled(
+            )))
     else:
-        print("cpu_math_library_num_threads: {}".format(config.cpu_math_library_num_threads()))
+        print("cpu_math_library_num_threads: {}".format(
+            config.cpu_math_library_num_threads()))
         if (args.use_mkldnn):
             print("enable_mkldnn: {}".format(config.mkldnn_enabled()))
     print("----------------------- Perf info -----------------------")
-    print("Average latency(ms): {}, QPS: {}".format(total_time/args.repeats, (args.repeats * args.batch_size) / (total_time/1000)))
+    print("Average latency(ms): {}, QPS: {}".format(total_time / args.repeats, (
+        args.repeats * args.batch_size) / (total_time / 1000)))
 
 
 if __name__ == "__main__":
     run_demo()
-

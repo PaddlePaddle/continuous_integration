0414fa2a0bae8c0085601531b1d6fbd8d3cec57d
lipeihan01 <lipeihan01@baidu.com>
2020-12-30 12:27:24 +0800

    tensorflow python benchamrk

--- inference/inference_benchmark/Python-benchmark/TensorFlow/clas_benchmark.py
+++ inference/inference_benchmark/Python-benchmark/TensorFlow/clas_benchmark.py
@@ -1,4 +1,6 @@
+import os
 import time
+import threading
 
 import numpy as np
 import tensorflow.compat.v1 as tf
@@ -32,38 +34,38 @@ def Inference(args, config) -> int:
                          "fp16" : trt.TrtPrecisionMode.FP16,
                          "int8" : trt.TrtPrecisionMode.INT8}
 
-    with tf.device(place):
-        with tf.Session(config=config) as sess:
-            # First deserialize your frozen graph:
-            with tf.io.gfile.GFile(model_path, 'rb') as f:
-                frozen_graph = tf.GraphDef()
-                frozen_graph.ParseFromString(f.read())
-            # Now you can create a TensorRT inference graph from your
-            use_calib = True if args.trt_precision == "int8" else False
-            if args.use_trt:
-                converter = trt.TrtGraphConverter(
-                    input_graph_def=frozen_graph,
-                    max_batch_size=args.batch_size,
-                    precision_mode=trt_precision_map[args.trt_precision],
-                    use_calibration=use_calib,
-                    nodes_blacklist=[output_node])
-                trt_graph = converter.convert()
-                output_node = tf.import_graph_def(
-                    trt_graph,
-                    return_elements=[output_node])
-            else:
-                output_node = tf.import_graph_def(
-                    frozen_graph,
-                    return_elements=[output_node])
+    # with tf.device(place):
+    with tf.Session(config=config) as sess:
+        # First deserialize your frozen graph:
+        with tf.io.gfile.GFile(model_path, 'rb') as f:
+            frozen_graph = tf.GraphDef()
+            frozen_graph.ParseFromString(f.read())
+        # Now you can create a TensorRT inference graph from your
+        use_calib = True if args.trt_precision == "int8" else False
+        if args.use_trt:
+            converter = trt.TrtGraphConverter(
+                input_graph_def=frozen_graph,
+                max_batch_size=args.batch_size,
+                precision_mode=trt_precision_map[args.trt_precision],
+                use_calibration=use_calib,
+                nodes_blacklist=[output_node])
+            trt_graph = converter.convert()
+            output_node = tf.import_graph_def(
+                trt_graph,
+                return_elements=[output_node])
+        else:
+            output_node = tf.import_graph_def(
+                frozen_graph,
+                return_elements=[output_node])
 
-            for i in range(args.warmup_times):
-                sess.run(output_node, {input_node : input_data})
+        for i in range(args.warmup_times):
+            sess.run(output_node, {input_node : input_data})
 
-            time1 = time.time()
-            for i in range(args.repeats):
-                sess.run(output_node, {input_node : input_data})
-            time2 = time.time()
-            total_inference_cost = (time2 - time1) * 1000  # total latency, ms
+        time1 = time.time()
+        for i in range(args.repeats):
+            sess.run(output_node, {input_node : input_data})
+        time2 = time.time()
+        total_inference_cost = (time2 - time1) * 1000  # total latency, ms
 
     return total_inference_cost
 
@@ -71,6 +73,11 @@ def run_demo():
     """
     run_demo
     """
+    from py_mem import record_from_pid
+    record_thread = threading.Thread(target=record_from_pid, args=(os.getpid(), ))
+    record_thread.setDaemon(True)
+    record_thread.start()
+    
     args = helper.parse_args()
     config = helper.prepare_config(args)
     total_time = Inference(args, config)

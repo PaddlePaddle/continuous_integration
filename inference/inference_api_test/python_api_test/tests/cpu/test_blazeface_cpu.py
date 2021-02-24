3b6e42cf0c65246a50a94b0f060f36c596d799fd
lipeihan01 <lipeihan01@baidu.com>
2021-02-24 19:20:43 +0800

    change pytest style

--- inference/inference_api_test/python_api_test/tests/cpu/test_blazeface_cpu.py
+++ inference/inference_api_test/python_api_test/tests/cpu/test_blazeface_cpu.py
@@ -25,30 +25,33 @@ import numpy as np
 from test_cpu_helper import TestModelInferenceCPU
 
 
-class TestBlazeFaceInferenceCPU(TestModelInferenceCPU):
+# class TestBlazeFaceInferenceCPU(TestModelInferenceCPU):
+#     """
+#     TestModelInferenceCPU
+#     Args:
+#     Returns:
+#     """
+
+TestBase = TestModelInferenceCPU()
+
+
+@pytest.mark.p0
+def test_inference_blazeface_cpu():
     """
-    TestModelInferenceCPU
+    Inference and check value
+    blazeface cpu model
     Args:
+        None
     Returns:
+        None
     """
+    model_name = "blazeface_nas_128"
+    tmp_path = os.path.join(TestBase.model_root, "Detection")
+    model_path = os.path.join(tmp_path, model_name, "model")
+    data_path = os.path.join(tmp_path, model_name, "data/data.json")
+    delta = 0.0001
 
-    @pytest.mark.p0
-    def test_inference_blazeface_cpu(self):
-        """
-        Inference and check value
-        blazeface cpu model
-        Args:
-            None
-        Returns:
-            None
-        """
-        model_name = "blazeface_nas_128"
-        tmp_path = os.path.join(self.model_root, "Detection")
-        model_path = os.path.join(tmp_path, model_name, "model")
-        data_path = os.path.join(tmp_path, model_name, "data/data.json")
-        delta = 0.0001
+    res, exp = TestBase.get_infer_results(model_path, data_path)
 
-        res, exp = self.get_infer_results(model_path, data_path)
-
-        for i in range(len(res)):
-            self.check_data(res[i].flatten(), exp[i].flatten(), delta)
+    for i in range(len(res)):
+        TestBase.check_data(res[i].flatten(), exp[i].flatten(), delta)

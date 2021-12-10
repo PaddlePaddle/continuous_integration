588b5600db36e301f0c48c7d54f0160fe1d3b2a0
wangye707 <39162106+wangye707@users.noreply.github.com>
2021-12-10 14:05:58 +0800

    Update xpu_ppyolo_r50vd_test.py

--- inference/inference_api_test/python_api_test/tests/xpu/xpu_ppyolo_r50vd_test.py
+++ inference/inference_api_test/python_api_test/tests/xpu/xpu_ppyolo_r50vd_test.py
@@ -19,13 +19,14 @@ import numpy as np
 import image_preprocess
 from PIL import Image
 from paddle.inference import Config
+from paddle.inference import PrecisionType
 from paddle.inference import create_predictor
 from test_src import test_gpu_model_jetson
 
 
 def inference_ppyolo_r50vd(img, model_path, params_path):
     """
-    inference_ttfnet
+    inference_ppyolo_r50vd
     Args:
         img: numpy img
         model_path: model path
@@ -35,7 +36,8 @@ def inference_ppyolo_r50vd(img, model_path, params_path):
     """
     batch_size = 1
     config = Config(model_path, params_path)
-    config.enable_use_gpu(0)
+    config.enable_xpu(10 * 1024 * 1024)
+    config.enable_lite_engine(PrecisionType.Float32, True) 
     config.switch_ir_optim(True)
     config.switch_use_feed_fetch_ops(False)
     config.switch_specify_input_names(True)

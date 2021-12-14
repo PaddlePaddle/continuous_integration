3e57c4cf1f61263fcdcb71e101da2b432476caa8
chalsliu <craziecharles@gmail.com>
2021-12-14 16:33:02 +0800

    Fix bugs

--- tipc/tipc_run.sh
+++ tipc/tipc_run.sh
@@ -8,11 +8,11 @@ for config_file in `find . -name "*train_infer_python.txt"`; do
     for mode in $test_mode; do
         mode=$(echo $mode | xargs)
         echo "==START=="$config_file"_"$mode
-        echo "CUDA_VISIBLE_DEVICES="$CUDA_VISIBLE_DEVICES"======="
+        echo "CUDA_VISIBLE_DEVICES="$CUDA_VISIBLE_DEVICES
         bash test_tipc/prepare.sh $config_file $mode
         bash test_tipc/test_train_inference_python.sh $config_file $mode
-        mv test_tipc/output "test_tipc/output_"$(echo $config_file | tr "/" "_")"_"$mode || echo "move output error"
-        mv test_tipc/output "test_tipc/data"$(echo $config_file | tr "/" "_")"_"$mode || echo "move data error"
+        mv test_tipc/output "test_tipc/output_"$(echo $config_file | tr "/" "_")"_"$mode || echo "move output error on "`pwd`
+        mv test_tipc/data "test_tipc/data"$(echo $config_file | tr "/" "_")"_"$mode || echo "move data error on "`pwd`
         echo "==END=="$config_file"_"$mode
     done
 done

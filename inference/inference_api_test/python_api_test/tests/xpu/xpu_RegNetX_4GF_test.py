9357845d756acb916defb502bd4cf065c2f3c470
wangye707 <39162106+wangye707@users.noreply.github.com>
2021-12-16 11:49:33 +0800

    Update xpu_RegNetX_4GF_test.py

--- inference/inference_api_test/python_api_test/tests/xpu/xpu_RegNetX_4GF_test.py
+++ inference/inference_api_test/python_api_test/tests/xpu/xpu_RegNetX_4GF_test.py
@@ -75,7 +75,7 @@ def test_RegNetX_4GF():
     Returns:
         None
     """
-    diff_standard = 2e-6
+    diff_standard = 2e-5
     model_name = "RegNetX_4GF"
     test_model = test_gpu_model_jetson(model_name=model_name)
     model_path, params_path = test_model.test_comb_model_path("cv_class_model")

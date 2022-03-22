bcb8af0d2cba25a0d2c0bdff1d061553dfcedd81
ZhangYulongg <1272816783@qq.com>
2022-03-22 12:25:46 +0800

    update upload script

--- tipc/tipc_run.sh
+++ tipc/tipc_run.sh
@@ -12,11 +12,11 @@ for config_file in `find . -name "*train_infer_python.txt"`; do
         bash test_tipc/prepare.sh $config_file $mode
         bash test_tipc/test_train_inference_python.sh $config_file $mode
         bash -x upload.sh ${config_file} ${mode} || echo "upload model error on"`pwd`
-        mv test_tipc/output "test_tipc/output_"$(echo $config_file | tr "/" "_")"_"$mode || echo "move output error on "`pwd`
-        mv test_tipc/data "test_tipc/data"$(echo $config_file | tr "/" "_")"_"$mode || echo "move data error on "`pwd`
         if [[ "$CHECK_LOSS" == "True" ]]; then
             sh check_loss.sh
         fi
+        mv test_tipc/output "test_tipc/output_"$(echo $config_file | tr "/" "_")"_"$mode || echo "move output error on "`pwd`
+        mv test_tipc/data "test_tipc/data"$(echo $config_file | tr "/" "_")"_"$mode || echo "move data error on "`pwd`
         echo "==END=="$config_file"_"$mode
     done
 done

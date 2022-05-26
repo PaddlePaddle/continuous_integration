9b27a6baca10b0420a505128a7f3f7de3944d7c7
zhengya01 <zhengya01@baidu.com>
2022-05-26 17:27:45 +0800

    add distribution

--- tipc/run.sh
+++ tipc/run.sh
@@ -1,5 +1,6 @@
 ROOT_PATH=${ROOT_PATH:-/mnt/xly/work}
-DOCKER_IMAGE=${DOCKER_IMAGE:-registry.baidubce.com/paddlepaddle/paddle:latest-dev-cuda10.1-cudnn7-gcc82}
+#DOCKER_IMAGE=${DOCKER_IMAGE:-registry.baidubce.com/paddlepaddle/paddle:latest-dev-cuda10.1-cudnn7-gcc82}
+DOCKER_IMAGE=${DOCKER_IMAGE:-registry.baidu.com/paddlecloud/base-images:paddlecloud-ubuntu18.04-gcc8.2-cuda11.0-cudnn8}
 PADDLE_WHL=${PADDLE_WHL:-https://paddle-qa.bj.bcebos.com/paddle-pipeline/Debug_GpuAll_LinuxUbuntu_Gcc82_Cuda10.1_Trton_Py37_Compile_H_DISTRIBUTE_Release/latest/paddlepaddle_gpu-0.0.0-cp37-cp37m-linux_x86_64.whl}
 
 work_dir=${ROOT_PATH}/${REPO}
@@ -25,7 +26,7 @@ wget -q --no-proxy -O $PWD/bce_whl.tar.gz  https://paddle-docker-tar.bj.bcebos.c
 if [[ ${CHAIN} == "chain_distribution" ]]
 then
     cd ${REPO}
-    cp -r continuous_integration/tipc/* .
+    cp -r ../continuous_integration/tipc/* .
     sh tipc_run.sh ${REPO} ${CHAIN} ${DOCKER_IMAGE} ${PADDLE_WHL} ${CODE_BOS} 
 else
     sh tipc.sh ${REPO} ${CHAIN}

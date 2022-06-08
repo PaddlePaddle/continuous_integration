4f9f3781d86d3b66461401d0727bfe426379659d
zhengya01 <zhengya01@baidu.com>
2022-06-08 16:53:56 +0800

    update chain_base

--- tipc/tipc_run.sh
+++ tipc/tipc_run.sh
@@ -197,7 +197,7 @@ fi
 # 确定链条的txt、mode、timeout
 case $CHAIN in
 chain_base) 
-    file_txt=train_infer_python.txt
+    file_txt=*train_infer_python.txt
     mode=lite_train_lite_infer
     time_out=600
     ;;

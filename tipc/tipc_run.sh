2a2d76072a73cf6ccf36bad94a0f21f8498fd1eb
zhengya01 <zhengya01@baidu.com>
2022-07-04 10:20:24 +0800

    update timeout

--- tipc/tipc_run.sh
+++ tipc/tipc_run.sh
@@ -243,7 +243,7 @@ case $CHAIN in
 chain_base) 
     file_txt=*train_infer_python.txt
     mode=lite_train_lite_infer
-    time_out=600
+    time_out=700
     ;;
 chain_infer_cpp)
     file_txt=*_infer_cpp_*

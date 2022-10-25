1b316d114dcae6cd8afb9286a898bbd62b902264
zhengya01 <zhengya01@baidu.com>
2022-10-25 10:25:53 +0800

    update tipc.sh

--- tipc/tipc.sh
+++ tipc/tipc.sh
@@ -173,14 +173,18 @@ if [[ $REPO == "PaddleNLP" ]]; then
     python -m pip install --retries 10 seqeval
     python -m pip install --retries 10 paddlenlp
 fi
-if [[ $REPO == "PaddleOCR" ]] && [[ $CHAIN == "chain_pact_infer_python" ]]; then
+if [[ $REPO == "PaddleOCR" ]]; then
     python -m pip install --retries 10 seqeval
     python -m pip install --retries 10 paddlenlp
 fi
-if [[ $REPO == "PaddleOCR" ]] && [[ $CHAIN == "chain_ptq_infer_python" ]]; then
-    python -m pip install --retries 10 seqeval
-    python -m pip install --retries 10 paddlenlp
-fi
+#if [[ $REPO == "PaddleOCR" ]] && [[ $CHAIN == "chain_pact_infer_python" ]]; then
+#    python -m pip install --retries 10 seqeval
+#    python -m pip install --retries 10 paddlenlp
+#fi
+#if [[ $REPO == "PaddleOCR" ]] && [[ $CHAIN == "chain_ptq_infer_python" ]]; then
+#    python -m pip install --retries 10 seqeval
+#    python -m pip install --retries 10 paddlenlp
+#fi
 if [[ $REPO == "PaddleVideo" ]]; then
     python -m pip install --retries 10 seqeval
     python -m pip install --retries 10 paddlenlp

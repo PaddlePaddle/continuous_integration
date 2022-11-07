0b59e89ea36fa172952df57df6c6c5bc1d2b41e4
zhengya01 <zhengya01@baidu.com>
2022-11-07 19:22:53 +0800

    test

--- tipc/tipc_run.sh
+++ tipc/tipc_run.sh
@@ -52,11 +52,13 @@ function run()
     kill -9 $watchdog  >/dev/null 2>&1
 }
 
+NEED_ICAFE=1
+
 function run_model()
 {
     config_file=$1
     mode=$2
-    case $CHAIN in
+    case $chain in
     chain_base)
         if [[ $TF == "True" ]]; then
             bash test_export_shell.sh $config_file
@@ -65,12 +67,15 @@ function run_model()
         last_status=${PIPESTATUS[0]}
         if [[ ${last_status} -ne 0 ]]
         then
+           #if NEED_ICAFE==1:
+           #create icafe
+           #NEED_ICAFE=0
            exit ${last_status}
         fi
         bash test_tipc/test_train_inference_python.sh $config_file $mode 
         ;;
     chain_infer_cpp)
-        if [[ $REPO == PaddleDetection ]]; then
+        if [[ $repo == PaddleDetection ]]; then
             bash test_tipc/prepare.sh $config_file $mode $PADDLE_INFERENCE_TGZ
             last_status=${PIPESTATUS[0]}
             if [[ ${last_status} -ne 0 ]]
@@ -78,7 +83,7 @@ function run_model()
                exit ${last_status}
             fi
             bash test_tipc/test_inference_cpp.sh $config_file $PADDLE_INFERENCE_TGZ '1'
-        elif [[ $REPO == PaddleClas ]]; then
+        elif [[ $repo == PaddleClas ]]; then
             bash test_tipc/prepare.sh $config_file $mode $PADDLE_INFERENCE_TGZ
             last_status=${PIPESTATUS[0]}
             if [[ ${last_status} -ne 0 ]]
@@ -162,15 +167,15 @@ function run_model()
         bash test_tipc/test_train_inference_python.sh $config_file $mode
         ;;
     *)
-        echo "CHAIN must be chain_base chain_infer_cpp chain_amp chain_serving_cpp chain_serving_python chain_paddle2onnx chain_distribution chain_pact_infer_python chain_ptq_infer_python"
-        echo "$CHAIN not supported at the moment"
+        echo "chain must be chain_base chain_infer_cpp chain_amp chain_serving_cpp chain_serving_python chain_paddle2onnx chain_distribution chain_pact_infer_python chain_ptq_infer_python"
+        echo "$chain not supported at the moment"
         exit 2
         ;;
  
     esac
 }
 
-#if [[ $REPO == "PaddleOCR" ]]; then
+#if [[ $repo == "PaddleOCR" ]]; then
 #sed -i "s/GPUID=\$2/GPUID=\$3/g" test_tipc/test_serving_infer_python.sh
 #sed -i "s/web_service|pipeline/web_service/g" test_tipc/test_serving_infer_python.sh
 #sed -i "s/ps ux/#ps ux/g" test_tipc/test_serving_infer_python.sh
@@ -182,21 +187,21 @@ function run_model()
 #sed -i '150 i             eval $stop_cmd' test_tipc/test_serving_infer_python.sh
 #fi
 
-if [[ ${REPO} == "PaddleOCR" ]]
+if [[ ${repo} == "PaddleOCR" ]]
 then
 #sed -i '192 i if [ ! -d "paddle_inference" ]; then' test_tipc/test_inference_cpp.sh
 #sed -i '193 i ln -s paddle_inference_install_dir paddle_inference' test_tipc/test_inference_cpp.sh
 #sed -i '194 i fi' test_tipc/test_inference_cpp.sh
 sed -i 's#https://github.com/LDOUBLEV/AutoLog.git#https://gitee.com/Double_V/AutoLog#g' deploy/cpp_infer/external-cmake/auto-log.cmake
 sed -i 's#https://github.com/LDOUBLEV/AutoLog.git#https://gitee.com/Double_V/AutoLog#g' test_tipc/prepare.sh
-elif [[ ${REPO} == "PaddleClas" ]]
+elif [[ ${repo} == "PaddleClas" ]]
 then
 sed -i 's#https://github.com/LDOUBLEV/AutoLog.git#https://gitee.com/Double_V/AutoLog#g' deploy/cpp/external-cmake/auto-log.cmake
 else
 echo ""
 fi
 
-if [[ ${REPO} == "PaddleDetection" ]]
+if [[ ${repo} == "PaddleDetection" ]]
 then
 sed -i 's/sleep 2s/sleep 10s/g' test_tipc/test_serving_infer_python.sh
 fi
@@ -205,21 +210,21 @@ fi
 mkdir -p test_tipc/output
 touch TIMEOUT
 touch RESULT
-if [[ $CHAIN == "chain_paddle2onnx" ]]; then
+if [[ $chain == "chain_paddle2onnx" ]]; then
     pip install onnx==1.9.0
     pip install paddle2onnx
     pip install onnxruntime
 fi
 
-if [[ $CHAIN == "chain_serving_python" ]]; then
-  if [[ $REPO != "PaddleOCR" ]]; then
+if [[ $chain == "chain_serving_python" ]]; then
+  if [[ $repo != "PaddleOCR" ]]; then
     pip install paddle-serving-server-gpu==0.9.0.post102
     pip install paddle_serving_client==0.9.0
     pip install paddle-serving-app==0.9.0
   fi
 fi
 
-if [[ $CHAIN == chain_serving_cpp ]]; then
+if [[ $chain == chain_serving_cpp ]]; then
         # 安装client 和 app
         pip install paddle_serving_client==0.9.0
         pip install paddle-serving-app==0.9.0
@@ -268,19 +273,20 @@ if [[ $CHAIN == chain_serving_cpp ]]; then
         export CUDA_PATH='/usr/local/cuda'
         export CUDNN_LIBRARY='/usr/local/cuda/lib64/'
         export CUDA_CUDART_LIBRARY='/usr/local/cuda/lib64/'
-        export TENSORRT_LIBRARY_PATH='/usr/local/TensorRT6-cuda10.1-cudnn7/targets/x86_64-linux-gnu/'
+        #export TENSORRT_LIBRARY_PATH='/usr/local/TensorRT6-cuda10.1-cudnn7/targets/x86_64-linux-gnu/'
+        export TENSORRT_LIBRARY_PATH='/usr/local/TensorRT-6.0.1.8/targets/x86_64-linux-gnu/'
         cd ..
 
         # cp 自定义op代码
-        if [[ $REPO == PaddleSeg ]]
+        if [[ $repo == PaddleSeg ]]
         then
             rm -f ${Serving_repo_path}/core/general-server/op/general_clas_op.*
             cp test_tipc/serving_cpp/general_seg_op.* ${Serving_repo_path}/core/general-server/op
-        elif [[ $REPO == PaddleDetection ]]
+        elif [[ $repo == PaddleDetection ]]
         then
             cp deploy/serving/cpp/preprocess/*.h ${Serving_repo_path}/core/general-server/op
             cp deploy/serving/cpp/preprocess/*.cpp ${Serving_repo_path}/core/general-server/op
-        elif [[ $REPO == PaddleClas ]]
+        elif [[ $repo == PaddleClas ]]
         then
             cp deploy/paddleserving/preprocess/general_clas_op.* ${Serving_repo_path}/core/general-server/op
             cp deploy/paddleserving/preprocess/preprocess_op.* ${Serving_repo_path}/core/predictor/tools/pp_shitu_tools
@@ -319,7 +325,7 @@ if [[ $CHAIN == chain_serving_cpp ]]; then
 fi
 
 # 确定链条的txt、mode、timeout
-case $CHAIN in
+case $chain in
 chain_base) 
     file_txt=*train_infer_python.txt
     mode=lite_train_lite_infer
@@ -366,8 +372,8 @@ chain_ptq_infer_python)
     time_out=1800
     ;;
 *)
-    echo "CHAIN must be chain_base chain_infer_cpp chain_amp chain_serving_cpp chain_serving_python chain_paddle2onnx chain_distribution chain_pact_infer_python chain_ptq_infer_python"
-    echo "$CHAIN not supported at the moment"
+    echo "chain must be chain_base chain_infer_cpp chain_amp chain_serving_cpp chain_serving_python chain_paddle2onnx chain_distribution chain_pact_infer_python chain_ptq_infer_python"
+    echo "$chain not supported at the moment"
     exit 1
     ;;
 esac
@@ -375,11 +381,11 @@ esac
 # 确定套件的待测模型列表, 其txt保存到full_chain_list_all
 touch full_chain_list_all_tmp
 touch full_chain_list_all
-python model_list.py $REPO ${PWD}/test_tipc/configs/ $file_txt full_chain_list_all_tmp 
-#if [[ ${REPO} == "PaddleClas" ]]
+python model_list.py $repo ${PWD}/test_tipc/configs/ $file_txt full_chain_list_all_tmp 
+#if [[ ${repo} == "PaddleClas" ]]
 #then
 #sed -i "94 i ln -s paddle_inference_install_dir paddle_inference" ${PWD}/test_tipc/prepare.sh
-#python model_list.py $REPO ${PWD}/test_tipc/config/ $file_txt full_chain_list_all_tmp
+#python model_list.py $repo ${PWD}/test_tipc/config/ $file_txt full_chain_list_all_tmp
 #fi
 if [ ! ${grep_models} ]; then  
     grep_models=undefined
@@ -412,7 +418,7 @@ do
   IFS=$'\n'
   lines=(${dataline})
   model_name=$(func_parser_value "${lines[1]}")
-  if [[ $CHAIN == "chain_distribution" ]]
+  if [[ $chain == "chain_distribution" ]]
   then
     echo "==START=="$config_file
     JOB_NAME=tipc-${model_name}-${mode}
@@ -420,7 +426,7 @@ do
     PADDLE_WHL=$3
     docker_image="registry.baidu.com/paddlecloud/base-images:paddlecloud-ubuntu18.04-gcc8.2-cuda11.0-cudnn8"
     CODE_BOS=$6
-    bash pdc.sh ${JOB_NAME} ${REPO} ${PADDLE_WHL} ${docker_image} ${CODE_BOS} $config_file ${mode} ${time_out} >log.pdc 2>&1
+    bash pdc.sh ${JOB_NAME} ${repo} ${PADDLE_WHL} ${docker_image} ${CODE_BOS} $config_file ${mode} ${time_out} >log.pdc 2>&1
     pdc_job_id=`cat log.pdc | grep "jobId = job-" | awk -F ',' '{print $1}' | awk -F '= ' '{print $2}'`
     # todo 判断pdc任务是否提交成功
     echo ${model_name},${pdc_job_id} >> pdc_job_id
@@ -428,15 +434,15 @@ do
   else
     start=`date +%s`
     echo "==START=="$config_file
-    if [[ $CHAIN == "chain_base" ]] && [[ $REPO == "PaddleVideo" ]]
+    if [[ $chain == "chain_base" ]] && [[ $repo == "PaddleVideo" ]]
     then
         time_out=1200
     fi
-    #if [[ $CHAIN == "chain_serving_python" ]] && [[ $REPO == "PaddleDetection" ]]
+    #if [[ $chain == "chain_serving_python" ]] && [[ $repo == "PaddleDetection" ]]
     #then
     #    time_out=600
     #fi
-    if [[ $CHAIN == "chain_base" ]] || [[ $CHAIN == "chain_amp" ]]
+    if [[ $chain == "chain_base" ]] || [[ $chain == "chain_amp" ]]
     then
         if [[ $config_file =~ test_tipc/configs/.*_PACT/ ]] || [[ $config_file =~ test_tipc/configs/.*_KL/ ]]
         then
@@ -469,9 +475,9 @@ do
 
     if [[ "${DEBUG}" == "False" ]]
     then
-      bash -x upload.sh ${config_file} ${mode} ${CHAIN} $repo_commit $frame_commit || echo "upload model error on"`pwd`
+      bash -x upload.sh ${config_file} ${mode} ${chain} $repo_commit $frame_commit || echo "upload model error on"`pwd`
     fi
-    if [[ $REPO != PaddleSeg ]]
+    if [[ $repo != PaddleSeg ]]
     then
       #mv test_tipc/output "test_tipc/output_"$(echo $config_file | tr "/" "_")"_"$mode || echo "move output error on "`pwd`
       mv test_tipc/data "test_tipc/data"$(echo $config_file | tr "/" "_")"_"$mode || echo "move data error on "`pwd`
@@ -480,12 +486,12 @@ do
 done
 
 # watch_job_status and get log, job_id in file pdc_job_id
-if [[ "$CHAIN" == "chain_distribution" ]]
+if [[ "$chain" == "chain_distribution" ]]
 then
   export http_proxy=
   export https_proxy=
-  python get_pdc_job_result.py pdc_job_id $REPO
-  python get_pdc_job_log.py $REPO
+  python get_pdc_job_result.py pdc_job_id $repo
+  python get_pdc_job_log.py $repo
   
   
 fi
@@ -497,10 +503,10 @@ for f in `find . -name '*.log'`; do
 done
 
 # upload log, create icafe, write result to db
-python report.py ${REPO} ${CHAIN} ${sender} ${reciver} ${mail_proxy}
+python report.py ${repo} ${chain} ${sender} ${reciver} ${mail_proxy}
 if [[ "${DEBUG}" == "False" ]]
 then
-if [[ "$REPO" == "PaddleOCR" ]] || [[ "$REPO" == "PaddleSeg" ]] || [[ "$REPO" == "PaddleDetection" ]] || [[ "$REPO" == "PaddleClas" ]] || [[ "$REPO" == "PaddleVideo" ]] || [[ "$REPO" == "PaddleGAN" ]] || [[ "$REPO" == "PaddleNLP" ]]
+if [[ "$repo" == "PaddleOCR" ]] || [[ "$repo" == "PaddleSeg" ]] || [[ "$repo" == "PaddleDetection" ]] || [[ "$repo" == "PaddleClas" ]] || [[ "$repo" == "PaddleVideo" ]] || [[ "$repo" == "PaddleGAN" ]] || [[ "$repo" == "PaddleNLP" ]]
 then
     python writedb.py $task_dt $repo $repo_branch $repo_commit $chain $paddle_whl $frame_branch $frame_commit $docker_image $cuda_version $cudnn_version $python_version 
 fi
@@ -509,13 +515,13 @@ fi
 # update model_url latest
 if [[ "${DEBUG}" == "False" ]]
 then
-if [ -f "tipc_models_url_${REPO}_${CHAIN}.txt" ];then
+if [ -f "tipc_models_url_${repo}_${chain}.txt" ];then
     date_stamp=`date +%m_%d`
     push_file=./bce-python-sdk-0.8.27/BosClient.py
-    cp "tipc_models_url_${REPO}_${CHAIN}.txt" "tipc_models_url_${REPO}_${CHAIN}_latest.txt"
-    cp "tipc_models_url_${REPO}_${CHAIN}.txt" "tipc_models_url_${REPO}_${CHAIN}_${date_stamp}.txt"
-    python2 ${push_file} "tipc_models_url_${REPO}_${CHAIN}_latest.txt" paddle-qa/fullchain_ce_test/model_download_link
-    python2 ${push_file} "tipc_models_url_${REPO}_${CHAIN}_${date_stamp}.txt" paddle-qa/fullchain_ce_test/model_download_link
+    cp "tipc_models_url_${repo}_${chain}.txt" "tipc_models_url_${repo}_${chain}_latest.txt"
+    cp "tipc_models_url_${repo}_${chain}.txt" "tipc_models_url_${repo}_${chain}_${date_stamp}.txt"
+    python2 ${push_file} "tipc_models_url_${repo}_${chain}_latest.txt" paddle-qa/fullchain_ce_test/model_download_link
+    python2 ${push_file} "tipc_models_url_${repo}_${chain}_${date_stamp}.txt" paddle-qa/fullchain_ce_test/model_download_link
 fi
 fi
 

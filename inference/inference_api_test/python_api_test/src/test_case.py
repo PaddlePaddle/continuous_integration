232ee11e66090bf18f249741c98b306e243e320f
YuBaoku <49938469+EmmonsCurse@users.noreply.github.com>
2023-06-14 13:33:00 +0800

    remove config.enable_memory_optim() when config_type = 'mkldnn' (#286)

--- inference/inference_api_test/python_api_test/src/test_case.py
+++ inference/inference_api_test/python_api_test/src/test_case.py
@@ -126,7 +126,8 @@ class DeployConfig(object):
             raise Exception('Config type [%s] invalid!' % config_type)
         # predictor_config.switch_ir_optim(True) # default is enabled, no need to enable
         predictor_config.switch_specify_input_names(True)
-        predictor_config.enable_memory_optim()  # enable temporarily
+        if config_type != 'mkldnn':
+            predictor_config.enable_memory_optim()
         predictor_config.switch_use_feed_fetch_ops(False)
         return predictor_config
 
